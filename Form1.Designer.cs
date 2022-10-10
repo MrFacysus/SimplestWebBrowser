cefd269dee402b5c6418736b37f0900cd6abd640
Mateo Kladarić <52569856+MrFacysus@users.noreply.github.com>
2022-10-10 17:04:50 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SimpleWebBrowser
+{
+    partial class WebBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.ForwardButton = new System.Windows.Forms.Button();
+            this.UrlBar = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.HTMLPreview = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 45);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1880, 835);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.HTMLPreview);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1872, 809);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(12, 14);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(37, 23);
+            this.BackButton.TabIndex = 1;
+            this.BackButton.Text = "<";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // ForwardButton
+            // 
+            this.ForwardButton.Location = new System.Drawing.Point(45, 14);
+            this.ForwardButton.Name = "ForwardButton";
+            this.ForwardButton.Size = new System.Drawing.Size(35, 23);
+            this.ForwardButton.TabIndex = 2;
+            this.ForwardButton.Text = ">";
+            this.ForwardButton.UseVisualStyleBackColor = true;
+            this.ForwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
+            // 
+            // UrlBar
+            // 
+            this.UrlBar.Location = new System.Drawing.Point(87, 14);
+            this.UrlBar.Name = "UrlBar";
+            this.UrlBar.Size = new System.Drawing.Size(1312, 20);
+            this.UrlBar.TabIndex = 3;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(1405, 14);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(83, 23);
+            this.SearchButton.TabIndex = 4;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // HTMLPreview
+            // 
+            this.HTMLPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HTMLPreview.Location = new System.Drawing.Point(3, 3);
+            this.HTMLPreview.Name = "HTMLPreview";
+            this.HTMLPreview.Size = new System.Drawing.Size(1866, 803);
+            this.HTMLPreview.TabIndex = 0;
+            this.HTMLPreview.Text = "";
+            // 
+            // WebBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 892);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.UrlBar);
+            this.Controls.Add(this.ForwardButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "WebBrowser";
+            this.Text = "My WebBrowser";
+            this.Load += new System.EventHandler(this.WebBrowser_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button ForwardButton;
+        private System.Windows.Forms.TextBox UrlBar;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.RichTextBox HTMLPreview;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWebBrowser
+{
+    public partial class WebBrowser : Form
+    {
+        public WebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        // define public variables for future use
+        public static HttpClient Client = new HttpClient();
+        public static string LastUsedLink = "";
+        public static string CurrentlyUsedLink = "";
+
+        private void WebBrowser_Load(object sender, EventArgs e)
+        {
+            // check if preference.cfg exists
+            if (System.IO.File.Exists("preference.cfg"))
+            {
+                // read preference.cfg
+                string[] lines = System.IO.File.ReadAllLines("preference.cfg");
+                // check if preference.cfg has a link
+                if (lines.Length > 0)
+                {
+                    // set home page if it exists
+                    if (lines[0].Length > 0)
+                    {
+                        // assign the home page
+                        UrlBar.Text = lines[0];
+                        // remember the home page
+                        CurrentlyUsedLink = lines[0];
+                    }
+                }
+            }
+            else
+            {
+                // create preference.cfg
+                System.IO.File.Create("preference.cfg");
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            // if currentlyusedlink is not empty set it to the lastusedlink
+            if (CurrentlyUsedLink.Length > 0)
+            {
+                LastUsedLink = CurrentlyUsedLink;
+            }
+            // set currentlyusedlink to the urlbar text
+            CurrentlyUsedLink = UrlBar.Text;
+            // if the urlbar text is not empty Navigate the url
+            if (UrlBar.Text.Length > 0)
+            {
+                Navigate(UrlBar.Text);
+            }
+        }
+
+        // defining a navigate function
+        public void Navigate(string url)
+        {
+            // send http request
+            HttpResponseMessage response = Client.GetAsync(UrlBar.Text).Result;
+            // display the response
+            HTMLPreview.Text = response.Content.ReadAsStringAsync().Result;
+            // change the tab name to the title of the web page from the html element
+            tabControl1.SelectedTab.Text = HTMLPreview.Text.Substring(HTMLPreview.Text.IndexOf("<title>") + 7, HTMLPreview.Text.IndexOf("</title>") - HTMLPreview.Text.IndexOf("<title>") - 7);
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            // load the previous link
+            UrlBar.Text = LastUsedLink;
+            // navigate to previous link
+            Navigate(LastUsedLink);
+        }
+
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            // load the current link
+            UrlBar.Text = CurrentlyUsedLink;
+            // navigate to current link
+            Navigate(CurrentlyUsedLink);
+        }
+    }
+}
